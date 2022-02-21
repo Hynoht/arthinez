367f9458cd84f874e0c54c12b5f4ff479e675cf7
Hynoht <93657305+Hynoht@users.noreply.github.com>
2022-02-21 16:15:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const menu = document.getElementById('menu');
 const animation = document.getElementById('animation');
 const graph = document.getElementById('graph');
 const td = document.getElementById('td');
-const ac =document.querySelector('.ac');
-const ap =document.querySelector('.ap');
-const sv =document.querySelector('.sv');
-const ct =document.querySelector('.ct');
-const ac1 =document.querySelector('.ac1');
-const ap1 =document.querySelector('.ap1');
-const sv1 =document.querySelector('.sv1');
-const ct1 =document.querySelector('.ct1');
 //
 window.onscroll = function(){
     if(scrollY > 200) {
@@ -63,63 +55,4 @@ const fb = document.getElementById('fb');
         fb.classList.add('active');
     }
 });
-//
-ac.addEventListener('click', accueil);
-ac1.addEventListener('click',accueil);
 
-ap.addEventListener('click', propos);
-ap1.addEventListener('click', propos);
-
-sv.addEventListener('click', service);
-sv1.addEventListener('click', service);
-
-ct.addEventListener('click', contact);
-ct1.addEventListener('click', contact);
-
-function accueil() {
-    ac.classList.add('active');
-    ap.classList.remove('active');
-    sv.classList.remove('active');
-    ct.classList.remove('active');
-
-    ac1.classList.add('active');
-    ap1.classList.remove('active');
-    sv1.classList.remove('active');
-    ct1.classList.remove('active');
-}
-
-function propos() {
-    ap.classList.add('active');
-    ac.classList.remove('active');
-    sv.classList.remove('active');
-    ct.classList.remove('active');
-
-    ap1.classList.add('active');
-    ac1.classList.remove('active');
-    sv1.classList.remove('active');
-    ct1.classList.remove('active');
-}
-
-function service() {
-    sv.classList.add('active');
-    ap.classList.remove('active');
-    ac.classList.remove('active');
-    ct.classList.remove('active');
-
-    sv1.classList.add('active');
-    ap1.classList.remove('active');
-    ac1.classList.remove('active');
-    ct1.classList.remove('active');
-}
-
-function contact() {
-    ct.classList.add('active');
-    ap.classList.remove('active');
-    sv.classList.remove('active');
-    ac.classList.remove('active');
-
-    ct1.classList.add('active');
-    ap1.classList.remove('active');
-    sv1.classList.remove('active');
-    ac1.classList.remove('active');
-}
